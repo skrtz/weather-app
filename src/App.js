33eacf81bc63885d854d94801cdd342d72803917
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-11-13 12:40:43 -0500

    create weather location data fetch request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Weather from './components/weather';
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+      console.log('latitude is:', lat);
+      console.log('longitude is:', long);
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&long=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          setData(result);
+          console.log(result);
+        })
+    }
+    fetchData();
+  }, [lat, long]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(typeof data.main != 'undefined') ? (
+        <Weather weatherData={data}/>
+      ) : (
+        <div></div>
+      )}
+
     </div>
   );
 }
